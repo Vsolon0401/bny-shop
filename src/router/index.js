5fa4c3b29764a4e9f11ca7fcf1c3393661a72a97
Vsolon0401 <2390426297@qq.com>
2023-06-13 11:37:32 +0800

    feat: 静态资源添加

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Layout from '@/views/Layout/Layout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'layout',
+      component: Layout
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      // 路由懒加载
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login/Login.vue')
     }
   ]
 })
